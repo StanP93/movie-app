2fe0780f4ffe489c4c19c4d2c33c53afce781edb
Stanislau Pranovich <stanislaupranovich@MacBook-Air-Stanislau.local>
2022-01-10 16:17:36 +0300

    added component with state

--- src/App.js
+++ src/App.js
@@ -1,9 +1,26 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+import Movie from "./components/Movie";
 
+const API_HEAD = 'https://api.themoviedb.org/3/discover/movie?api_key=21f0d68db25663ca746bd45f66988281&language=en-US&sort_by=popularity.desc'
+const API_IMAGE = 'https://image.tmdb.org/t/p/w1280/'
+const API_SEARCH = 'https://api.themoviedb.org/3/search/movie?api_key=21f0d68db25663ca746bd45f66988281&query='
+  
 function App() {
+  const [movies, setMovies] = useState([]);
+
+  useEffect (() => {
+    fetch(API_HEAD)
+      .then((res)=>res.json())
+      .then((data)=> {
+        setMovies(data.results);
+      });
+  }, [])
+
   return (
     <div className="App">
-      
+      {movies.map((movie)=>(
+        <Movie data={movie} key={movie.id}/>
+      ))}
     </div>
   );
 }
---
+++ src/components/Movie.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import classes from './Movie.module.css'
+
+const Movie = (props) => {
+    return (
+        <div className={classes.myMovie}>
+            <img/>
+        </div>
+    )
+}
+
+export default Movie;
